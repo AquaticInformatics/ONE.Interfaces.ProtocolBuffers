5a1eabf5d7be1649dac438d25d4b8ef18130c895
Aaron Krone <akrone@hach.com>
2019-05-08 17:21:09 -0600

    Added Script, the transcript of the last run and new classes generated

---
+++ protogen-batch.ps1
@@ -0,0 +1,22 @@
+﻿$currentPath = pwd
+#$files = Get-ChildItem -Path . -Filter *.proto -Recurse | ForEach-Object {$_.FullName} 
+$files = Get-ChildItem -Path . -Filter *.proto -Recurse | ForEach-Object {"$_"} 
+$files = $files.Replace($currentPath.Path, ".")
+
+$protoDirs =  Get-ChildItem -Directory -Recurse | Where-Object {$_.FullName -inotmatch 'out'} | ForEach-Object {$_.FullName}
+$protoDirs = $protoDirs.Replace($currentPath.Path, ".") | ForEach-Object {"--proto_path=$_"}
+
+
+$protoDirsCommand = [String]::Join(" ", $protoDirs)
+
+$logName =  (Get-Date -DisplayHint Time).ToString().Replace(":","_").Replace("/","_").Replace(" ", "-")
+
+Start-Transcript $logName
+foreach($file in $files)
+{
+    $Command = "protogen --csharp_out=./out/ $protoDirsCommand $file"
+    echo `n$Command
+
+    Invoke-Expression -Command $Command
+}
+Stop-Transcript
