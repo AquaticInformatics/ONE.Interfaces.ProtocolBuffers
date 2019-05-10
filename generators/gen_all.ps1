4916e3bb0e6f8673fbdd82c1b92062723a844a19
Aaron Krone <akrone@hach.com>
2019-05-09 22:29:50 -0600

    Script to gen all languages supported by protoc

---
+++ generators/gen_all.ps1
@@ -0,0 +1,37 @@
+﻿$currentPath = pwd
+
+$files = Get-ChildItem -Path . -Filter *.proto -Recurse | ForEach-Object {"$_"} 
+$files = $files.Replace($currentPath.Path, ".")
+
+$protoDirs =  Get-ChildItem -Directory -Recurse | Where-Object {$_.FullName -inotmatch 'out'} | ForEach-Object {$_.FullName}
+$protoDirs = $protoDirs.Replace($currentPath.Path, ".") | ForEach-Object {"--proto_path=$_"}
+
+$protoDirsCommand = [String]::Join(" ", $protoDirs)
+
+
+$dirs = "cpp", "java", "js", "objc", "php", "python", "ruby"
+
+# Create Output Directories
+foreach($dir in $dirs) {
+    mkdir -Force ./out/$dir
+}
+
+$flags = @()
+foreach ($dir in $dirs){
+    $flags += "--$dir" +"_out=./out/$dir"
+}
+$flags = [String]::Join(" ", $flags)
+$flags
+
+$logName =  (Get-Date -DisplayHint Time).ToString().Replace(":","_").Replace("/","_").Replace(" ", "-")
+
+Start-Transcript $logName
+foreach($file in $files)
+{
+#    $Command = "protogen --csharp_out=./out/ $protoDirsCommand $file"
+    $Command = "protoc $flags $protoDirsCommand $file"
+    echo `n$Command
+
+    Invoke-Expression -Command $Command
+}
+Stop-Transcript
---
+++ proto/out/js/entitytype.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.claros.common.configuration.EntityType');
+
+/**
+ * @enum {number}
+ */
+proto.claros.common.configuration.EntityType = {
+  FORMTEMPLATE: 0
+};
+
