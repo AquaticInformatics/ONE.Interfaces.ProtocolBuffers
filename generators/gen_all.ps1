3a75de824638d0865abcf445e2c2632eb9b7b883
Dan Gossner <dgossner@hach.com>
2021-12-06 07:54:35 -0600

    renamed main folder to one

--- generators/gen_all.ps1
+++ generators/gen_all.ps1
@@ -1,6 +1,6 @@
 ﻿mkdir -Force ../proto/flat
 
-Get-ChildItem ../proto/aquarius -Recurse -Filter "*.proto" | Copy-Item -destination ../proto/flat
+Get-ChildItem ../proto/one -Recurse -Filter "*.proto" | Copy-Item -destination ../proto/flat
 
 cd ../proto/flat
 
